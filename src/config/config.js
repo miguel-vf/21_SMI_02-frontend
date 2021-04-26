57688ffe7be859975934fa19a7ccad17cebbe6eb
uo271747 <uo271747@uniovi.es>
2021-04-26 17:55:11 +0200

    mediaURL fixxed

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-const mediaURL = 'http://localhost:8080/'; //URL to access /videos and /images
+const mediaURL = 'http://localhost:8080'; //URL to access /videos and /images
 
 module.exports = {
     mediaURL,
